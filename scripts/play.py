ae140461243e98022e1566f61a678b506969a3aa
eRaquet <git.far@raquetfamily.com>
2024-12-21 16:56:46 -0500

    added model specific configurations to play.py

--- scripts/play.py
+++ scripts/play.py
@@ -7,64 +7,71 @@ import pygame as pg
 import time
 
 #initiate our current netowrk
-model = Network.Model(offset=0)
+model_default = Network.Model(offset=0)
 
 #creates the various player configuations (player type, player color)
 playerDic = {
     ('h', 'w') : Players.Human(),
     ('h', 'b') : Players.Human(),
-    ('b', 'w') : Players.Bot(1, model, confidence=5.0),
-    ('b', 'b') : Players.Bot(0, model, confidence=5.0),
-    ('r', 'w') : Players.Bot(1, model, confidence=0.0),
-    ('r', 'b') : Players.Bot(0, model, confidence=0.0)
+    ('b', 'w') : Players.Bot(1, model_default, confidence=5.0),
+    ('b', 'b') : Players.Bot(0, model_default, confidence=5.0),
+    ('r', 'w') : Players.Bot(1, model_default, confidence=0.0),
+    ('r', 'b') : Players.Bot(0, model_default, confidence=0.0)
 }
 
 playerTypes = ['h', 'b', 'r']
 
 while True:
 
-    print("Please indicate game configuration; '(white type) vs. (black type)' [human -> 'h', bot -> 'b', random -> 'r']: ", end='')
+    print("Please indicate game configuration; '(white type) vs. (black type)' [human -> 'h', bot 3 -> 'b3', random -> 'r']: ", end='')
     config = input()
-    
-    if len(config) == 7:
 
-        white = config[0]
-        black = config[6]
+    config = config.split(" vs. ")
+    white = config[0]
+    black = config[1]
 
-        #if the player types are valid
-        if white in playerTypes and black in playerTypes:
+    #if the player types are valid
+    if white[0] in playerTypes and black[0] in playerTypes:
 
-            game = Game.Game([playerDic[(black, 'b')], playerDic[(white, 'w')]])
-
-            while not game.isEnd():
-
-                #if a human is playing...
-                if white == 'h' or black == 'h':
-                    
-                    #...just play as usual
-                    game.makeMove()
-
-                #if its just bots...
-                else:
-
-                    #...wait for the operator to step to the next move
-                    for event in pg.event.get():
-
-                        if event.type == pg.MOUSEBUTTONDOWN:
-
-                            t = time.time()
-                            game.makeMove()
-                            print("move time: " + str(time.time() - t))
-
-            print("Do you want to play again? (y/n) ", end='')
-
-            if input() != 'y':
-                break
-
-        #invalid player types
+        if white[0] != 'b' or len(white) == 1:
+            whitePlayer = playerDic[(white, 'w')]
         else:
-            print("Do not recognize player types")
+            model = Network.Model(index=int(white[1:]))
+            whitePlayer = Players.Bot(1, model, confidence=5.0)
         
-    #invalid config statement
+        if black[0] != 'b' or len(black) == 1:
+            whitePlayer = playerDic[(black, 'w')]
+        else:
+            model = Network.Model(index=int(black[1:]))
+            blackPlayer = Players.Bot(0, model, confidence=5.0)
+
+        game = Game.Game([blackPlayer, whitePlayer])
+
+        while not game.isEnd():
+
+            #if a human is playing...
+            if white == 'h' or black == 'h':
+                
+                #...just play as usual
+                game.makeMove()
+
+            #if its just bots...
+            else:
+
+                #...wait for the operator to step to the next move
+                for event in pg.event.get():
+
+                    if event.type == pg.MOUSEBUTTONDOWN:
+
+                        t = time.time()
+                        game.makeMove()
+                        print("move time: " + str(time.time() - t))
+
+        print("Do you want to play again? (y/n) ", end='')
+
+        if input() != 'y':
+            break
+
+    #invalid player types
     else:
-        print("Invalid game configuration statement")
+        print("Do not recognize player types")
